8f7833c275e91c3f553d5ee710dde5bfdc940092
hTaw99 <hady.tawfik1999@gmail.com>
2024-09-24 21:22:58 +0300

    Change top color

--- components/layout/topbar.tsx
+++ components/layout/topbar.tsx
@@ -1,6 +1,6 @@
 export function Topbar() {
 	return (
-		<div className='bg-[#384B70] py-2 text-center text-white typography-R14'>
+		<div className='bg-green-500 py-2 text-center text-white typography-R14'>
 			Free shipping for any order above 712 EGP
 		</div>
 	);
