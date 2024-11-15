a9b1d6543a46cf540fc5fc09455320d39ee2467f
Hady Tawfik <hady.tawfik1999@gmail.com>
2024-11-15 17:10:55 +0200

    Adjust the categories section style

--- app/(home)/categories.tsx
+++ app/(home)/categories.tsx
@@ -46,11 +46,11 @@ export default async function Categories() {
 								key={cat._id}
 								className='group basis-1/2 cursor-pointer media-sm:basis-1/3 media-md:basis-1/4 media-lg:basis-1/6'>
 								<Link href={`/shop?category=${cat._id}`}>
-									<Card className='flex aspect-square flex-col items-center justify-center gap-4 p-2 text-[#bc6c25] transition-all group-hover:border-[#bc6c25]'>
-										<span className='transition-all group-hover:scale-110'>
+									<Card className='grid aspect-square grid-rows-[2fr_1fr] flex-col items-center justify-center gap-2 p-2 text-[#bc6c25] transition-all group-hover:border-[#bc6c25]'>
+										<span className='place-items-center transition-all group-hover:scale-110'>
 											{iconMapper[cat.slug as keyof typeof iconMapper]}
 										</span>
-										<span className='text-center text-gray-700'>
+										<span className='self-start text-center text-gray-700'>
 											{cat.name}
 										</span>
 									</Card>
