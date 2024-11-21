cb396c88e300aaf7184f71c3c6627246886dd568
Hady Tawfik <hady.tawfik1999@gmail.com>
2024-11-21 18:13:58 +0200

    Making the company page responsive

--- app/companies/[slug]/page.tsx
+++ app/companies/[slug]/page.tsx
@@ -35,35 +35,33 @@ export default async function CompanyPage(props: {
 		<section>
 			<div className='h-48 w-full bg-gray-30' />
 			<div className='container -mt-8'>
-				<div className='flex gap-4'>
+				<div className='flex flex-col gap-2 media-md:flex-row'>
 					<Avatar className='size-36 flex-shrink-0 rounded-full border-8 border-white bg-gray-30'>
 						<AvatarImage />
 						<AvatarFallback className='rounded-md bg-gray-30 text-gray-100'>
 							<BuildingPlaceholder size={64} />
 						</AvatarFallback>
 					</Avatar>
-					<div className='mt-10 [&>p]:leading-normal [&>p]:typography-R14'>
+					<div className='media-md:mt-10 [&>p]:leading-normal [&>p]:typography-R14'>
 						<div className='mb-1 capitalize text-black typography-B28'>
 							{company.name}
 						</div>
-						<div className='mb-3 flex items-center gap-4'>
+						<div className='mb-3 flex flex-row items-center gap-4'>
 							<RatingStars averageRating={2} />
 							<Separator
 								orientation='vertical'
 								className='h-4'
 							/>
-							<span className='flex items-center gap-1 text-gray-200 typography-R14'>
+							<span className='flex gap-1 text-gray-200 typography-R14'>
 								<CircleCheckIcon
 									size={18}
-									className='text-white'
+									className='flex-shrink-0 text-white'
 									fill='#76828d'
 								/>
 								150 orders in last week
 							</span>
 						</div>
-						{/* <span className='text-green-150 leading-snug typography-R14'> */}
 						{parse(company.description)}
-						{/* </span> */}
 					</div>
 				</div>
 
--- features/products/components/allergen.tsx
+++ features/products/components/allergen.tsx
@@ -5,24 +5,28 @@ export function Allergen() {
 	return (
 		<div className='border-b border-gray-50 pb-10'>
 			<p className='mb-4 typography-B16'>Allergen notice</p>
-			<div className='flex gap-4 text-green-500'>
+			<div className='flex gap-2 text-green-500 media-md:gap-4'>
 				<div className='flex flex-col items-center justify-center'>
-					<div className='mb-2 rounded-full border border-green-500 p-6'>
+					<div className='mb-2 rounded-full border border-green-500 p-4 media-md:p-6'>
 						<BeanOffIcon />
 					</div>
-					<span className='typography-M16'>Soy-free</span>
+					<span className='typography-M14 media-md:typography-M16'>
+						Soy-free
+					</span>
 				</div>
 				<div className='flex flex-col items-center justify-center'>
-					<div className='mb-2 rounded-full border border-green-500 p-6'>
+					<div className='mb-2 rounded-full border border-green-500 p-4 media-md:p-6'>
 						<WheatOffIcon />
 					</div>
-					<span className='typography-M16'>Gluten-free</span>
+					<span className='typography-M14 media-md:typography-M16'>
+						Gluten-free
+					</span>
 				</div>
 				<div className='flex flex-col items-center justify-center'>
-					<div className='mb-2 rounded-full border border-green-500 p-6'>
+					<div className='mb-2 rounded-full border border-green-500 p-4 media-md:p-6'>
 						<DnaOffIcon />
 					</div>
-					<span className='typography-M16'>No-gmo</span>
+					<span className='typography-M14 media-md:typography-M16'>No-gmo</span>
 				</div>
 			</div>
 		</div>
