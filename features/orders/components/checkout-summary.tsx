8dadc862a59375d07476b95952bbd38ccba2473b
hTaw99 <hady.tawfik1999@gmail.com>
2024-11-24 15:43:52 +0200

    Add Coupon feature

--- features/orders/components/checkout-summary.tsx
+++ features/orders/components/checkout-summary.tsx
@@ -4,7 +4,7 @@ import { Card } from 'components/ui/card';
 import CheckoutCartItem from 'app/checkout/checkout-cart-item';
 import { PlaceOrderBtn } from 'features/orders/components/place-order-btn';
 import { TCart } from 'features/cart/types/cart';
-import { Input } from 'components/ui/input';
+import { TextField } from 'components/ui/text-field';
 import { TicketIcon } from 'lucide-react';
 
 export function CheckoutSummary({
@@ -50,10 +50,24 @@ export function CheckoutSummary({
 					<span>0 EGP</span>
 				</div>
 			</div>
-			<div className='relative'>
-				<TicketIcon className='absolute left-2 top-2' />
-				<Input size='sm' />
+			<div className='mb-4 rounded-md border border-gray-30 bg-white p-1'>
+				<div className='relative overflow-hidden rounded-md bg-gradient-to-b from-green-light-50 p-3'>
+					<div className='absolute left-0 top-1/2 size-4 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white' />
+					<div className='absolute right-0 top-1/2 size-4 -translate-y-1/2 translate-x-1/2 rounded-full bg-white' />
+					<h4 className='mb-2 text-green-500 typography-SB14'>Discount code</h4>
+					<TextField
+						size='sm'
+						className='bg-white'
+						suffexIcon={
+							<button className='text-green-light-600 typography-SB12'>
+								Apply
+							</button>
+						}
+						prefixIcon={<TicketIcon className='text-green-light-600' />}
+					/>
+				</div>
 			</div>
+
 			<div className='mb-4 flex items-center justify-between text-green-light-700 typography-SB18'>
 				<p className='text-green-800'>Total Price</p>
 				{convertToReadableNumber(cart.totalPrice)} EGP
