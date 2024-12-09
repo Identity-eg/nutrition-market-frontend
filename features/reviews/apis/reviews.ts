91628803696a51b36e25a28149cea20fe520db23
Amr Tawfik <atawfik.c@saip.gov.sa>
2024-12-09 03:46:20 +0200

    review actions & setup sentry/nextjs

--- features/reviews/apis/reviews.ts
+++ features/reviews/apis/reviews.ts
@@ -1,8 +1,12 @@
 'use server';
 
-import { request } from 'apis/request';
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
+
+import { request } from 'apis/request';
 import type { TReview } from 'features/reviews/types/review';
+import { actionClient } from 'apis/action-clients';
+import { flattenValidationErrors } from 'next-safe-action';
 
 type TGetReviewsReturn = {
 	reviews: TReview[];
@@ -23,28 +27,6 @@ export const getReviews = async ({
 	return data;
 };
 
-type TAddReviewProps = Pick<TReview, 'comment' | 'title' | 'rating'> & {
-	productId: string;
-};
-
-type TUpdateReviewProps = Pick<TReview, 'comment' | 'title' | 'rating'> & {
-	reviewId: string;
-};
-
-export const addReview = async ({ productId, ...rest }: TAddReviewProps) => {
-	const data = await request({
-		url: `/reviews`,
-		method: 'POST',
-		body: {
-			product: productId,
-			...rest,
-		},
-	});
-
-	revalidatePath('/shop/[productId]', 'page');
-	return data;
-};
-
 export const getSingleReview = async ({ reviewId }: { reviewId: string }) => {
 	const data = await request({
 		url: `/reviews/${reviewId}`,
@@ -53,28 +35,89 @@ export const getSingleReview = async ({ reviewId }: { reviewId: string }) => {
 	return data;
 };
 
-export const deleteReview = async ({ reviewId }: { reviewId: string }) => {
-	const data = await request({
-		url: `/reviews/${reviewId}`,
-		method: 'DELETE',
-	});
-
-	revalidatePath('/shop/[productId]', 'page');
-	return data;
+type TUpdateReviewProps = Pick<TReview, 'comment' | 'title' | 'rating'> & {
+	reviewId: string;
 };
 
-export const updateReview = async ({
-	reviewId,
-	...rest
-}: TUpdateReviewProps) => {
-	const data = await request({
-		url: `/reviews/${reviewId}`,
-		method: 'PATCH',
-		body: {
-			...rest,
+const addReviewSchema = z.object({
+	productId: z.string().min(1, 'ProductId is required'),
+	rating: z.number().min(1, 'Rating is required').max(5, 'max Rating is 5'),
+	title: z.string().min(1, 'Review title is required'),
+	comment: z.string().min(1, {
+		message: 'Comment is required',
+	}),
+});
+
+export const addReview = actionClient
+	.metadata({ actionName: 'add-review-action' })
+	.schema(addReviewSchema, {
+		// handleValidationErrorsShape: ve => flattenValidationErrors(ve).fieldErrors,
+	})
+	.action(
+		async ({ parsedInput: { productId, ...rest } }) => {
+			const data = await request({
+				url: `/reviews`,
+				method: 'POST',
+				body: {
+					product: productId,
+					...rest,
+				},
+			});
+			return data;
 		},
-	});
+		{ onSettled: () => revalidatePath('/shop/[slug]', 'page') }
+	);
 
-	revalidatePath('/shop/[productId]', 'page');
-	return data;
-};
+// ############################################################
+const updateReviewSchema = z.object({
+	reviewId: z.string().min(1, 'ReviewId is required'),
+	rating: z.number().min(1, 'Rating is required').max(5, 'max Rating is 5'),
+	title: z.string().min(1, 'Review title is required'),
+	comment: z.string().min(1, {
+		message: 'Comment is required',
+	}),
+});
+
+export const updateReview = actionClient
+	.metadata({
+		actionName: 'update-review-action',
+	})
+	.schema(updateReviewSchema, {
+		// handleValidationErrorsShape: ve => flattenValidationErrors(ve).fieldErrors,
+	})
+	.action(
+		async ({ parsedInput: { reviewId, ...rest } }) => {
+			const data = await request({
+				url: `/reviews/${reviewId}`,
+				method: 'PATCH',
+				body: {
+					...rest,
+				},
+			});
+			return data;
+		},
+		{ onSettled: () => revalidatePath('/shop/[slug]', 'page') }
+	);
+
+// ######################################################
+const deleteReviewSchema = z.object({
+	reviewId: z.string().min(1, 'ReviewId is required'),
+});
+
+export const deleteReview = actionClient
+	.metadata({
+		actionName: 'delete-review-action',
+	})
+	.schema(deleteReviewSchema, {
+		// handleValidationErrorsShape: ve => flattenValidationErrors(ve).fieldErrors,
+	})
+	.action(
+		async ({ parsedInput: { reviewId } }) => {
+			const data = await request({
+				url: `/reviews/${reviewId}`,
+				method: 'DELETE',
+			});
+			return data;
+		},
+		{ onSettled: () => revalidatePath('/shop/[slug]', 'page') }
+	);
